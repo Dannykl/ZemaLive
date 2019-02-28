df82252938fa53e97aa9afc5619a7eaf26354c86
Daniel Niguse <Danielniguse8@gmail.com>
2019-02-28 12:13:39 +0000

    correct error

--- app/src/main/java/live/zema/app/data/model/AppConstants.java
+++ app/src/main/java/live/zema/app/data/model/AppConstants.java
@@ -3,7 +3,7 @@ package live.zema.app.data.model;
 public interface AppConstants {
 
     String PREF_NAME = "zema-live-streaming-platform";
-
+    String BASE_URL = "";
     String LOGGED_IN_PREF = "logged_in_status";
     String LOGGED_OUT = "logged_out";
     String SUCCESSFULLY_SIGNED = "Successfully signed up";
